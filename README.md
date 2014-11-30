5611e302617de9bcc63e598221f3b4a3626df8f5
Tobias Preuss <tobias.preuss@googlemail.com>
2014-11-30 14:16:25 +0100

    Add halfnarp-client library v.1.0.0.

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Halfnarp Client
+# Halfnarp Client Demo
+
+Android project to demonstrate the usage of the [Halfnarp client library][halfnarp-client]
+
+
+## Usage
+
+There is a **demo application module** to checkout a working a example.
+
+
+### Gradle build
+
+To install the demo application to your device run the following task:
+
+```bash
+$ ./gradlew installDebug
+```
+
+
+
+## Author
+
+* [Tobias Preuss][tobias-preuss]
+
+## License
+
+    Copyright 2014 Tobias Preuss
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+
+[halfnarp-client]: https://github.com/johnjohndoe/HalfnarpClient
+[tobias-preuss]: https://github.com/johnjohndoe
--- demo/src/main/java/info/metadude/android/halfnarpclient/demo/MainActivity.java
+++ demo/src/main/java/info/metadude/android/halfnarpclient/demo/MainActivity.java
@@ -2,6 +2,16 @@ package info.metadude.android.halfnarpclient.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+
+import java.util.List;
+
+import info.metadude.java.library.halfnarp.ApiModule;
+import info.metadude.java.library.halfnarp.TalkPreferencesService;
+import info.metadude.java.library.halfnarp.model.TalkPreferencesResponse;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -10,6 +20,28 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadData();
+    }
+
+    private void loadData() {
+        TalkPreferencesService service = ApiModule.getTalkPreferencesService();
+        service.getTalkPreferencesResponse(new Callback<List<TalkPreferencesResponse>>() {
+            @Override
+            public void success(
+                    List<TalkPreferencesResponse> talkPreferencesResponses,
+                    Response response) {
+                for (TalkPreferencesResponse talkPreferencesResponse : talkPreferencesResponses) {
+                    String title = talkPreferencesResponse.getTitle();
+                    int trackId = talkPreferencesResponse.getTrackId();
+                    Log.d(getClass().getName(), trackId + ": " + title);
+                }
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.e(getClass().getName(), "Error = " + error);
+            }
+        });
     }
 
 }
